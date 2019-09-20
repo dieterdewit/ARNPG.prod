b5170380de7ff552d6c38336d43f8bba9dec6537
Christopher Sandoval <san13660@uvg.edu.gt>
2019-09-19 18:19:11 -0600

    Funcionalidad basica mapa avistamientos

--- ARNPG/src/app/mapa-avistamientos/mapa-avistamientos.page.ts
+++ ARNPG/src/app/mapa-avistamientos/mapa-avistamientos.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder,NativeGeocoderOptions,NativeGeocoderResult } from '@ionic-native/native-geocoder/ngx';
+
+declare var google;
 
 @Component({
   selector: 'app-mapa-avistamientos',
@@ -7,9 +11,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaAvistamientosPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  address:string;
+  coords:string;
+
+  geoLatitude: number;
+  geoLongitude: number;
+  geoAccuracy:number;
+  geoAddress: string;
+
+  constructor(
+    private geolocation: Geolocation,
+    private nativeGeocoder: NativeGeocoder
+  ) { }
 
   ngOnInit() {
+    this.loadMap();
   }
 
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.getAddressFromCoords(resp.coords.latitude, resp.coords.longitude);
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+      this.map.addListener('tilesloaded', () => {
+        console.log('accuracy',this.map);
+        this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
+        this.coords = "Lat: " + this.map.center.lat() + " Long: " + this.map.center.lng();
+      });
+
+      this.setMarkers(latLng);
+ 
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  setMarkers(latLng){
+    var contentString = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h1 id="firstHeading" class="firstHeading">Ocelote</h1>'+
+    '<div id="bodyContent">'+
+    '<p><b>Avistamiento</b>, vi un gran ocelote. Se llama Otis. ' +
+    '</div>'+
+    '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    let marker = new google.maps.Marker({
+      position: latLng,
+      map: this.map,
+      title: 'Hello World!'
+    });
+
+    marker.addListener('click', function() {
+      infowindow.open(this.map, marker);
+    });
+  }
+ 
+  getAddressFromCoords(lattitude, longitude) {
+    console.log("getAddressFromCoords "+lattitude+" "+longitude);
+    let options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+ 
+    this.nativeGeocoder.reverseGeocode(lattitude, longitude, options)
+      .then((result: NativeGeocoderResult[]) => {
+        this.address = "";
+        let responseAddress = [];
+        for (let [key, value] of Object.entries(result[0])) {
+          if(value.length>0)
+          responseAddress.push(value);
+ 
+        }
+        responseAddress.reverse();
+        for (let value of responseAddress) {
+          this.address += value+", ";
+        }
+        this.address = this.address.slice(0, -2);
+      })
+      .catch((error: any) =>{ 
+        this.address = "Dirección no disponible.";
+      });
+  }
 }
