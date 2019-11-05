aedeaae2a151d3a3da5b70f6333f17a9ba35f5c9
JUMPSTONIK <xtineen@gmail.com>
2019-11-04 23:55:45 -0600

    informacion de los pop ops listo

--- src/app/especie/especie.page.ts
+++ src/app/especie/especie.page.ts
@@ -19,6 +19,10 @@ export class EspeciePage implements OnInit {
   validations_form: FormGroup;
   image: any;
   item: any;
+  infoNums: String = "";
+  info2L: String = "";
+  infoIs: String = "";
+  info1L: String = "";
   load: boolean = false;
 
   constructor(
@@ -59,53 +63,74 @@ export class EspeciePage implements OnInit {
       this.item = data;
 
       if(this.item.lea == "1"){
-        this.item.lea = "1"
+        this.item.lea = "1";
+        this.infoNums = "Casi Extinta";
       }else if(this.item.lea == "2"){
         this.item.lea = "2"
+        this.infoNums = "En Grave peligro de extincion";
       }else if(this.item.lea == "3"){
         this.item.lea = "3"
+        this.infoNums = "Manejo especial / Uso controlado";
       }else if (this.item.lea == "X"){
         this.item.lea = "X"
+        this.infoNums = "No esta en la lista roja de fauna CONAP";
       }else{
         this.item.lea = "-"
+        this.infoNums = "Informacion no disponible";
       }
 
       if(this.item.cites == "I"){
         this.item.cites = "I"
+        this.infoIs = "Especie con mayor grado de peligro";
       }else if(this.item.cites == "II"){
         this.item.cites = "II"
+        this.infoIs = "Especie no amenazada, pero puede estarlo";
       }else if(this.item.cites == "III"){
         this.item.cites = "III"
+        this.infoIs = "Especie protegida al menos en un pais";
       }else if (this.item.cites == "X"){
         this.item.cites = "X"
+        this.infoIs = "Especie no protegida por CITES";
       }else{
         this.item.cites = "-"
+        this.infoIs = "Informacion no disponible";
       }
 
       if(this.item.uicn == "LC"){
         this.item.uicn = "LC"
+        this.info2L = "Poca preocupacion de extincion";
       }else if(this.item.uicn == "VU"){
         this.item.uicn = "VU"
+        this.info2L = "Vulnerable a extincion";
       }else if(this.item.uicn == "EN"){
         this.item.uicn = "EN"
+        this.info2L = "En peligro de extincion";
       }else if (this.item.uicn == "CR"){
         this.item.uicn = "CR"
+        this.info2L = "En peligro critico de extincion";
       }else if (this.item.uicn == "DD"){
         this.item.uicn = "DD"
+        this.info2L = "Datos insuficientes";
       }else{
         this.item.uicn = "-"
+        this.info2L = "Informacion no disponible";
       }
 
       if(this.item.distEstacional == "R"){
         this.item.distEstacional = "R"
+        this.info1L = "Especie residente o que no migra";
       }else if(this.item.distEstacional == "M"){
         this.item.distEstacional = "M"
+        this.info1L = "Especie migratoria";
       }else if(this.item.distEstacional == "T"){
         this.item.distEstacional = "T"
+        this.info1L = "Especie transitoria";
       }else if (this.item.distEstacional == "ER"){
         this.item.distEstacional = "ER"
+        this.info1L = "Especie endemiga regional";
       }else{
         this.item.distEstacional = "-"
+        this.info1L = "Informacion no disponible";
       }
     }
     else{
